05ab0f5400789baebcc6ea380056eb371a936f33
Mykhailo <0660330233@ukr.net>
2017-08-13 12:33:16 +0300

    First programm

---
+++ file1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+  int dogs;
+  printf("How meny dogs do you have?\n");
+  scanf("%d", &dogs);
+  printf("Ok! You have %d dogs in your family\n", dogs);
+  return 0;
+}
